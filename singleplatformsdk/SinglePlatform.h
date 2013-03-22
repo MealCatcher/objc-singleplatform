cb98b8e9c5fb2a25f260c057f557bb05c5f6b4fa
jorgeastorga <jeastorga@gmail.com>
2013-03-22 16:04:04 -0700

    added the .gitignore file to the repo

---
+++ singleplatformsdk/SinglePlatform.h
@@ -0,0 +1,13 @@
+//
+//  SinglePlatform.h
+//  singleplatformsdk
+//
+//  Created by Jorge Astorga on 3/22/13.
+//  Copyright (c) 2013 Jorge Astorga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SinglePlatform : NSObject
+
+@end
---
+++ singleplatformsdk/SinglePlatform.m
@@ -0,0 +1,13 @@
+//
+//  SinglePlatform.m
+//  singleplatformsdk
+//
+//  Created by Jorge Astorga on 3/22/13.
+//  Copyright (c) 2013 Jorge Astorga. All rights reserved.
+//
+
+#import "SinglePlatform.h"
+
+@implementation SinglePlatform
+
+@end
